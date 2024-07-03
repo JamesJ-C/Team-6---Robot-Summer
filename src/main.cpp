c36900344eedf5a1e64f729526d220fb0d1f1aac
JamesJ-C <james@hjcoffinco.com>
2024-07-03 14:15:11 -0700

    UART set-up

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
+#include <HardwareSerial.h>
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 	-1 // This display does not have a reset pin accessible
