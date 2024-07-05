730baa7b4d929c5afb248c0b5d7dea19f1b52106
JamesJ-C <james@hjcoffinco.com>
2024-07-05 11:15:48 -0700

    Motor class

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,56 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 
 
 
+class Motor {
+
+    private:
+    PinName PWM_pinA;
+    PinName PWM_pinB;
+
+
+    public:
+
+    Motor() = default;
+
+
+    PinName getPinA(){
+
+      return PWM_pinA;
+    }
+
+    PinName getPinB(){
+
+      return PWM_pinB;
+    }
+
+    Motor(PinName PWM_pinA, PinName L_PWM_pinB) : PWM_pinA(PWM_pinA), PWM_pinB(L_PWM_pinB) {}
+
+    void forward(int val){
+
+      pwm_start(PWM_pinA, MOTOR_FREQUENCY, val, RESOLUTION_12B_COMPARE_FORMAT);
+      pwm_start(PWM_pinB, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
+
+    }
+
+    void backward(int val){
+
+      pwm_start(PWM_pinA, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
+      pwm_start(PWM_pinB, MOTOR_FREQUENCY, val, RESOLUTION_12B_COMPARE_FORMAT);
+
+    }
+
+     void stop(){
+
+      pwm_start(PWM_pinA, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
+      pwm_start(PWM_pinB, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
+
+    }
+
+};
+
+
+Motor motor1(PB_0, PB_1);
+
 void setup() {
 
   display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -39,14 +89,44 @@ void setup() {
 
   pinMode(POT_PIN, INPUT_ANALOG);
 
-  pinMode(MOTOR_A, OUTPUT);
-  pinMode(MOTOR_B, OUTPUT);
+//  pinMode(MOTOR_A, OUTPUT);
+  //pinMode(MOTOR_B, OUTPUT);
+
+
+  pinMode(motor1.getPinA(), OUTPUT);
+  pinMode(motor1.getPinB(), OUTPUT);
+
 
 
 }
 
 void loop() {
+
+  display_handler.clearDisplay();
+  display_handler.setTextSize(1);
+  display_handler.setTextColor(SSD1306_WHITE);
+  display_handler.setCursor(0,0);
+  display_handler.println("loop");
+  display_handler.display();
+
+  motor1.stop();
+  delay(100);
+
+  motor1.forward(800);
+  delay(3000);
+
+  motor1.stop();
+  delay(100);
+
+  motor1.backward(800);
+  delay(300);
+
+
+
   // put your main code here, to run repeatedly:
+
+
+  /*
 
   int potVal = analogRead(POT_PIN);
 
@@ -116,4 +196,5 @@ for (int i=0; i< 100000; i++){
 // */
 
 }
+
 
