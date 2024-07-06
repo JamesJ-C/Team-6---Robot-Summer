0035fef67ccf8581e8bee09ab4b6c572f0141406
JamesJ-C <james@hjcoffinco.com>
2024-07-06 00:23:20 -0700

    working ish

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-
 #include <vector>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -12,26 +12,8 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 #define IRSENSOR PA0
 #define THRESHOLD 100 // Black line detection threshold
 #define NUM_SAMPLES 200
-int sampleWave[NUM_SAMPLES];
-int sample;
-unsigned long currentTime;
-unsigned long startTime;
-void handle_state_change();
 
-int loopNum=0;
-
-
-/*  Function Decs */
-
-void convolution (int* measuredArray, int measuredArraySize, int* sineArray, 
-                    int sineArraySize, int* convolvedArray, int convolvedArraySize);
-
-double irAmplitude (int* measuredArray, int measuredArraySize, int timeStep);
-
-int findMax(int* array, int arraySize);
-int findMin(int* array, int arraySize);
-double crossCorelate(double* measuredWave, int measuredWaveSize, double deltaT, int numSamples);
-
+int loopCount = 0;
 
 void setup() {
   display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -57,56 +39,161 @@ void setup() {
 
   pinMode(PB11, OUTPUT);
   digitalWrite(PB11, LOW);
+  delay(100);
+
+  digitalWrite(PB11, HIGH);
+  delay(100);
   // Set up interrupt function
   //attachInterrupt(digitalPinToInterrupt(REFLECTANCE), handle_state_change, CHANGE);
-  sample = 0;
+
 }
 
 void loop() {
 
-
+loopCount++;
 //double measuredWave[3*numSamples];
 
   std::vector<double> IRsignal;
 
   int numSamples = 0;
-  int finishTime = 0;
-  int StartTime = millis();
-  while (millis() - startTime < 10){
+  unsigned long finishTime = 0;
+  unsigned long startTime = millis(); 
 
+    // display_handler.clearDisplay();
+    // display_handler.setTextSize(1);
+    // display_handler.setTextColor(SSD1306_WHITE);
+    // display_handler.setCursor(0,0);
+    // display_handler.print("before while ");
+    // //display_handler.print();
+    // display_handler.println();
+    // display_handler.display();
+    // delay(500);
+
+
+
+/*
+  Serial.println("outside loop");
+
+  display_handler.clearDisplay();
+  display_handler.setTextSize(1);
+  display_handler.setTextColor(SSD1306_WHITE);
+  display_handler.setCursor(0,0);
+  display_handler.print("startTime: ");
+  display_handler.print(startTime);
+  display_handler.println();
+  display_handler.display();  
+
+
+  while(millis() - startTime < 10) {
+
+    digitalWrite(PB11, LOW);
+
+
+
+
+  } */
+
+  //digitalWrite(PB11, HIGH);
+
+ // /*
+  while (millis() - startTime < 10){
+    //digitalWrite(PB11, LOW);
     IRsignal.push_back(analogRead(IRSENSOR));
     numSamples++;
     finishTime = millis();
 
-    display_handler.clearDisplay();
-    display_handler.setTextSize(1);
-    display_handler.setTextColor(SSD1306_WHITE);
-    display_handler.setCursor(0,0);
-    display_handler.print("numSamples: ");
-    display_handler.println(numSamples);
-    display_handler.display();
+    // display_handler.clearDisplay();
+    // display_handler.setTextSize(1);
+    // display_handler.setTextColor(SSD1306_WHITE);
+    // display_handler.setCursor(0,0);
+    // display_handler.print("numSamples: ");
+    // display_handler.println(numSamples);
+
+    // // display_handler.print("loop count: ");
+    // // display_handler.println(loopCount);
+    // display_handler.display();
 
   }
+
+  //   display_handler.clearDisplay();
+  //   display_handler.setTextSize(1);
+  //   display_handler.setTextColor(SSD1306_WHITE);
+  //   display_handler.setCursor(0,0);
+  //   display_handler.print("numSamples: ");
+  //   display_handler.println(numSamples);
+
+
+  //   display_handler.display();
+
+  // delay(500);
+
+
+    // display_handler.clearDisplay();
+    // display_handler.setTextSize(1);
+    // display_handler.setTextColor(SSD1306_WHITE);
+    // display_handler.setCursor(0,0);
+    // display_handler.print("after while");
+    // display_handler.println(micros());
+
+    // display_handler.print(startTime);
+    // display_handler.print(" ");
+    // display_handler.println(finishTime);
+
+    // display_handler.print(startTime - finishTime);
+
+    // display_handler.println();
+    // display_handler.display();
+    //delay(500);
+
+  //digitalWrite(PB11, HIGH);
+
+
 
 
   double oneK[2* numSamples] = {0};
   double oneKCorr[numSamples] = {0};
 
-  double oneKT = (double) numSamples / (double) (startTime - finishTime);
+  //double oneKT = (double) numSamples / (double) (startTime - finishTime);
+
+
+  int dt = ( finishTime - startTime );
+  double oneKT = (double) numSamples / ( (double) dt );
 
   for(int i = 0; i < 2 * numSamples;  i++) {
   
-    oneK[i] = sin(i * TWO_PI * oneKT);
+    oneK[i] = sin(i * TWO_PI / oneKT);
   
   }
+
+    // display_handler.clearDisplay();
+    // display_handler.println("here");
+
+    // display_handler.display();
+
+
+  // for (int i = 0; i < IRsignal.size(); i++){
+
+  //   display_handler.clearDisplay();
+  //   display_handler.setCursor(0,0);
+  //   display_handler.print(i);
+  //   display_handler.print(", ");
+  //   display_handler.println(IRsignal.at(i));
+  //   display_handler.display();
+  //   delay(500);
+
+  // }
 
   for (int k = 0; k < numSamples; k++){
 
     oneKCorr[k] = 0;
 
     for (int i = 0; i < numSamples; i++){
+      
+      //oneKCorr[k] = IRsignal.at(i);
+      
       oneKCorr[k] += IRsignal.at(i) * oneK[k+i];
     }
+
   }
 
   double max = oneKCorr[0];
@@ -123,11 +210,14 @@ void loop() {
     display_handler.setTextSize(1);
     display_handler.setTextColor(SSD1306_WHITE);
     display_handler.setCursor(0,0);
-    display_handler.print("startTime: ");
-    display_handler.println(startTime);
+    // display_handler.print("startTime: ");
+    // display_handler.println(startTime);
 
-    display_handler.print("finishTime: ");
-    display_handler.println(finishTime);
+    // display_handler.print("finishTime: ");
+    // display_handler.println(finishTime);
+
+    display_handler.print("dt: ");
+    display_handler.println(dt);
 
     display_handler.print("numSamples: ");
     display_handler.println(numSamples);
@@ -135,329 +225,16 @@ void loop() {
     display_handler.print("oneKT: ");
     display_handler.println(oneKT);
 
-    display_handler.println("cross correlation max: ");
-    display_handler.println(max);
+    display_handler.println("cc max: ");
+    display_handler.println(max,0);
 
     display_handler.display();
 
 
-  /*for(int i=0; i<numSamples; i++){
+   // if (max < -0.00)
+      //delay(2500);
 
-  
-    digitalWrite(PB11, LOW);
-    //delay(100);
-
-    measuredWave[i] = analogRead(REFLECTANCE);
-
-    display_handler.clearDisplay();
-    display_handler.setTextSize(1);
-    display_handler.setTextColor(SSD1306_WHITE);
-    display_handler.setCursor(0,0);
-    display_handler.println("i: ");
-    display_handler.println(i);
-    //display_handler.display();
-
-  } */
-
-/*
-
-  int timeF = millis();
-
-  double deltaT = (double) numSamples / (double) (timeF - time0);
-
-  double CC = crossCorelate(measuredWave, 3*numSamples, deltaT, numSamples);
-
-
-    display_handler.clearDisplay();
-    display_handler.setTextSize(1);
-    display_handler.setTextColor(SSD1306_WHITE);
-    display_handler.setCursor(0,0);
-    display_handler.print("time0: ");
-    display_handler.println(time0);
-
-    display_handler.print("timeF: ");
-    display_handler.println(timeF);
-
-    display_handler.print("deltaT: ");
-    display_handler.println(deltaT);
-
-    display_handler.println("cross correlation: ");
-    display_handler.println(CC);
-
-    display_handler.display();
-
-
-
-/*
-loopNum++;
-sample = -1;
-
-  int startTime = millis();
-  int sampleNum = 0;
-  while (sampleNum < NUM_SAMPLES){
-
-    sampleWave[sampleNum] = analogRead(PA0);
-    sampleNum++;
-  }
-  int finishTime = millis();
-
-  int sampleTime = finishTime - startTime;
-
-  int ConvVal = irAmplitude(sampleWave, NUM_SAMPLES, sampleTime/NUM_SAMPLES);
-
-  for (int i = 0; i < NUM_SAMPLES; i++) {
-    sampleWave[i] = 0;
-  }
-
-  display_handler.clearDisplay();
-  display_handler.setCursor(0,0);
-  display_handler.print("conv. val: ");
-  display_handler.println(ConvVal);
-
-  display_handler.print("loopNum: ");
-  display_handler.println(loopNum);
-
-  display_handler.print("Max in sample: ");
-  display_handler.println(findMax(sampleWave, NUM_SAMPLES));
-
-  display_handler.print("Min in sample: ");
-  display_handler.println(findMin(sampleWave, NUM_SAMPLES));
-
-  display_handler.display();
-
-
-/**
-  if (sample == NUM_SAMPLES) {
-    currentTime = millis();
-    int timeStep = (currentTime - startTime) / NUM_SAMPLES;
-    
-    loopNum++;
-
-    double ConvVal = irAmplitude(sampleWave, NUM_SAMPLES, timeStep);
-
-    display_handler.clearDisplay();
-    display_handler.setCursor(0,0);
-    display_handler.print("conv. val: ");
-    display_handler.println(ConvVal);
-
-    display_handler.print("loopNum: ");
-    display_handler.println(loopNum);
-
-    display_handler.print("Max in sample: ");
-    display_handler.println(findMax(sampleWave, NUM_SAMPLES));
-
-    display_handler.print("Min in sample: ");
-    display_handler.println(findMin(sampleWave, NUM_SAMPLES));
-
-
-    display_handler.display();
-    
-    // call james' function
-    // display intensity of 1 kHz and 10 kHz
-    // restart sample sequence
-    for (int i = 0; i < NUM_SAMPLES; i++) {
-      sampleWave[i] = 0;
-    }
-    sample = 0;
-  }
-
-  if (sample == 0) {
-    startTime = millis();
-  }
-
- // currentTime = millis();
-  // sampleWave[sample] = analogRead(PA0);
-  // sample++;
-
-
-*/
-}
-
-
-/**
- * @brief Returns the max double in an array
- * 
- * @param array 
- * @param arraySize 
- * @return double 
- */
-double findMax(double* array, int arraySize) {
-
-  double max = array[0];
-  for (int i = 1; i <arraySize; i++) {
-
-    if (array[i] > max){
-      max = array[i];
-    }
-
-  }
-
-  return max;
+   // */
 
 }
 
-/**
- * @brief returns the min double in an array
- * 
- * @param array 
- * @param arraySize 
- * @return int 
- */
-double findMin(double* array, int arraySize) {
-
-  double min = array[0];
-  for (int i = 1; i <arraySize; i++) {
-
-    if (array[i] < min){
-      min = array[i];
-    }
-
-  }
-
-  return min;
-
-}
-
-
-/**
- * @brief Creates a sin wave
- * 
- * @param sineWave 
- * @param period 
- * @param sampleSize the size of the sineWave array sent
- */
-void sinWave(double* sineWave, double period, int arraySize){
-
-  for (int i=0; i<=arraySize; i++){
-    
-    sineWave[i] = sin( (double) ( i * 2*PI / (double) period ) );
-
-  }
-
-}
-
-/**
- * @brief 
- * 
- * @param measuredWave wave measured
- * @param measuredWaveSize size of the measured wave
- * @param numSamples 
- * @return double 
- */
-double crossCorelate(double* measuredWave, int measuredWaveSize, double deltaT, int numSamples){
-
-
-  int correlate[numSamples];
-  double sinWaveArray[measuredWaveSize*2];
-
-  /*  Create sin wave  */
-  sinWave(sinWaveArray, deltaT, measuredWaveSize*2);
-
-  /*  Do correlation  */
-  for (int k=0; k < numSamples; k++){
-
-    correlate[k]=0;
-
-    for (int i=0; i < numSamples; i++){
-
-      correlate[k] += measuredWave[k]*sinWaveArray[i+k];
-    }
-  }  
-
-  //double max = findMax(correlate, measuredWaveSize);
-  //double min = findMin(correlate, measuredWaveSize);
-
-
-
-  double max = 0.00;//correlate[0];
-  for (int i = 1; i <numSamples; i++) {
-
-    if (correlate[i] > max){
-      max = correlate[i];
-    }
-
-  }
-
-  return max;
-}
-
-
-
-
-void handle_state_change() {
-  display_handler.clearDisplay();
-  display_handler.setCursor(0,0);
-  display_handler.println("Reflectance value2:\n");
-  //display_handler.println(analogRead(REFLECTANCE));
-  display_handler.display();
-}
-
-
-/**
- * 
- * @param measuredArray: the array of values measured from the IR sensor
- * @param measuredArraySize: the size of the measured array
- * 
- * @returns 'amount' of 1 kHz frequency in the measured array wave
- * 
- * 
-*/
-
-double irAmplitude (int* measuredArray, int measuredArraySize, int timeStep){
-
-
-  /** Create Sine Wave  **/
-  int convolvedArraySize = measuredArraySize*2;
-  const int frequency = 1000;
-  int sineArraySize = convolvedArraySize;
-  int sineArray[sineArraySize];
-
-
-  int convolvedArray[convolvedArraySize];
-  convolvedArray[0] = 0;
-
-  /*  Create Sine wave  */
-  for (int x = 0; x < sineArraySize; x++) {
-
-    sineArray[x] = sin(2*PI*frequency*x*timeStep);
-    Serial.println(sineArray[x]);
-
-  }
-
-  convolution(measuredArray, measuredArraySize, sineArray, sineArraySize, convolvedArray, convolvedArraySize);
-
-  if (convolvedArray[0] == 0){
-    return -9999999;
-  }
-
-  return findMax(convolvedArray, convolvedArraySize);
-
-
-}
-
-
-void convolution (int* measuredArray, int measuredArraySize, int* sineArray, 
-                    int sineArraySize, int* convolvedArray, int convolvedArraySize) {
-
-
-//200 = measured array size
-//  int measuredArray[200];
-  //int sinArray[400];
-  //int convolvedArray[200];
-
-  for (int offset = 0; offset <= measuredArraySize; offset++){ // offset of 0 is 1 overlap, offset of
-
-    for (int i = 0; i <= offset; i++){ // < or <= offset???
-
-      //offset of 0 corresponds to 1 overlapping value, namely, the first value of the measured, 
-      //and last value of the created 
-      convolvedArray[offset] = measuredArray[i] *sineArray[sineArraySize-offset+i];
-
-    }
-
-  }
-
-  //return convolvedArray;
-
-
-}
