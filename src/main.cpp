5e21cb8015f891e82542eb06caddeebaa7e4b0be
JamesJ-C <james@hjcoffinco.com>
2024-07-09 23:13:40 -0700

    somewhat working

--- src/main.cpp
+++ src/main.cpp
@@ -18,157 +18,23 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 #define ROTARY_B PB9
 
 
-/*  analog inputs */
-#define POT_PIN A1
+  // Rotary Encoder Inputs
+#define CLK PB8
+#define DT PB9
 
-#define MOTOR_FREQUENCY 1000
-
-/*  Motors  */
-
-
-/*  Dont need these definitions anymore   */
-//#define MOTOR_A PB_0
-//#define MOTOR_B PB_1
+int counter = 0;
+int currentStateCLK;
+int currentStateDT;
+int lastStateCLK;
+String currentDir ="";
 
 
-/**
-* assumes forwardDirection and backward direction are never both true
-* PWM_pinA and PWM_pinB should not be mutable
-* 
-* Uses the #define motor frequency
-*
- */
-
-class Motor {
-
-  private:
-  PinName PWM_pinA;
-  PinName PWM_pinB;
-
-  int motorSpeed = 0;
-
-  bool forwardDirection = false;
-  bool backwardDirection = false;
-
-
-  public:
-
-  /**
-   * @brief Construct a new Motor object with no PWM pins
-   * 
-   */
-  Motor() = default;
-
-  /**
-   * @brief Construct a new Motor object
-   * 
-   * @param PWM_pinA first PWM pin controlling the motor
-   * @param L_PWM_pinB Second PWM pin controlling the motor
-   */
-  Motor(PinName PWM_pinA, PinName L_PWM_pinB) : PWM_pinA(PWM_pinA), PWM_pinB(L_PWM_pinB) {}
-
-  /** 
-   * @brief Returns the first of 2 PWM pins
-   */
-  PinName getPinA(){
-    return PWM_pinA;
-  }
-
-  /**
-   * @brief Returns the second of the 2 PWM pins
-   */
-  PinName getPinB(){
-
-    return PWM_pinB;
-  }
-
-  /**
-   * @brief moves the motor forward at a given pwm signal
-   * 
-   * @param PWM_Val PWM to send to the motor 
-   */
-  void forward(int PWM_Val){
-    forwardDirection = true;
-    backwardDirection = false;
-
-    this->motorSpeed = PWM_Val;
-
-    pwm_start(PWM_pinA, MOTOR_FREQUENCY, PWM_Val, RESOLUTION_12B_COMPARE_FORMAT);
-    pwm_start(PWM_pinB, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-
-  }
-
-  /**
-   * @brief moves the motor backward at a given pwm signal
-   * 
-   * @param PWM_Val PWM to send to the motor 
-   */
-  void backward(int PWM_Val){
-    forwardDirection = false;
-    backwardDirection = true;
-    pwm_start(PWM_pinA, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-    pwm_start(PWM_pinB, MOTOR_FREQUENCY, PWM_Val, RESOLUTION_12B_COMPARE_FORMAT);
-  }
-
-
-  /**
-   * @brief Stops the motor from turning. If the motor is spinning, it pulses quickly in the opposite direction
-   * before sending nothing to the motors
-   * 
-   */
-  void stop(){
-  
-    if (forwardDirection){
-      pwm_start(PWM_pinA, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-      pwm_start(PWM_pinB, MOTOR_FREQUENCY, this->motorSpeed, RESOLUTION_12B_COMPARE_FORMAT);
-
-      delay(100);
-    }
-    else if (backwardDirection){
-      pwm_start(PWM_pinA, MOTOR_FREQUENCY, this->motorSpeed, RESOLUTION_12B_COMPARE_FORMAT);
-      pwm_start(PWM_pinB, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-
-      delay(100);    
-    }
-
-    pwm_start(PWM_pinA, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-    pwm_start(PWM_pinB, MOTOR_FREQUENCY, 0, RESOLUTION_12B_COMPARE_FORMAT);
-    
-  }
-
-
-};
-
-
-/*  create servo object  */
-Servo servo1;
-
-/*  create motor object   */
-Motor motor1(PB_0, PB_1);
-
-
-volatile u_int16_t rotations = 0;
-volatile u_int16_t steps = 0;
-
-
-void rotaryEncoder();
+void updateEncoder();
 
 void setup() {
 
-  /*  Encoder setup  */
-  //attachInterrupt(digitalPinToInterrupt(PB8), rotaryEncoder, FALLING);
-  pinMode(INPUT, ROTARY_A);
-  pinMode(INPUT, ROTARY_B);
-
-
-  /*  Servo setup  */
-  pinMode(PA8, OUTPUT);
-  servo1.attach(PA8);
-
-
   /*  Display setup  */
   display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
- 
 
   display_handler.display();
   delay(2000);
@@ -181,188 +47,157 @@ void setup() {
   display_handler.println("Setting up...");
   display_handler.display();
 
+	// Set encoder pins as inputs
 
-  /*  Motor Pins  */
-  pinMode(motor1.getPinA(), OUTPUT);
-  pinMode(motor1.getPinB(), OUTPUT);
 
-  /*  Other pins  */
-  pinMode(POT_PIN, INPUT_ANALOG);
+	pinMode(CLK,INPUT);
+	pinMode(DT,INPUT);
 
-  display_handler.clearDisplay();
-  display_handler.setTextSize(1);
-  display_handler.setTextColor(SSD1306_WHITE);
-  display_handler.setCursor(0,0);
-  display_handler.print("150 degrees");
-  display_handler.display();
+	// Setup Serial Monitor
+	Serial.begin(9600);
 
-  servo1.write(150);
-  delay(1000);
+	// Read the initial state of CLK
+	lastStateCLK = digitalRead(CLK);
+
+  attachInterrupt(digitalPinToInterrupt(CLK), updateEncoder, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(DT), updateEncoder, CHANGE);
 
 
 }
-
-bool previousRotaryAState = true;
-bool previousRotaryBState = false;
-int counter = 0;
-String dir = "";
-bool currentAState = 1;
-bool currentBState = 1;
-
 
 void loop() {
 
-
-  display_handler.clearDisplay();
-  display_handler.setTextSize(1);
-  display_handler.setTextColor(SSD1306_WHITE);
-  display_handler.setCursor(0,0);
-
-  currentAState = digitalRead(ROTARY_A);
-  currentBState = digitalRead(ROTARY_B);
-
-  display_handler.print("digital read: ");
-  display_handler.println(currentAState);
+    display_handler.clearDisplay();
+    display_handler.setTextSize(1);
+    display_handler.setTextColor(SSD1306_WHITE);
+    display_handler.setCursor(0,0);
+		display_handler.print("Direction: ");
+		display_handler.println(currentDir);
+		display_handler.print("Counter: ");
+		display_handler.println(counter);
+    display_handler.display();
 
 
+  // //For the non-interupt code      
+	// // Read the current state of CLK
+	// currentStateCLK = digitalRead(CLK);
 
-  if( currentAState == LOW && previousRotaryAState == HIGH) {
-
-    if (digitalRead(ROTARY_B) == currentAState){
-      counter++;
-      dir = "direction 1";
-    }
-    else {
-      counter--;
-      dir = "direction 2";
-    }
-  }
-
-  previousRotaryAState = currentAState;
+	// // If last and current state of CLK are different, then pulse occurred
+	// // React to only 1 state change to avoid double count
+	// if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
 
 
 
+  //   display_handler.clearDisplay();
+  //   display_handler.setTextSize(1);
+  //   display_handler.setTextColor(SSD1306_WHITE);
+  //   display_handler.setCursor(0,0);
+	// 	display_handler.print("In if function ");
+	// 	display_handler.println();
+	// 	display_handler.println("");
+	// 	display_handler.println();
+
+  //   display_handler.display();
 
 
-  display_handler.print("current A state: ");
-  display_handler.println(currentAState);
-
-  display_handler.print("previous A state: ");
-  display_handler.println(previousRotaryAState);
-
-
-
-  display_handler.print("steps: ");
-  display_handler.println(counter);
-  display_handler.println(dir);
-  display_handler.display();
+	// 	// If the DT state is different than the CLK state then
+	// 	// the encoder is rotating CCW so decrement
+	// 	if (digitalRead(DT) != currentStateCLK) {
+	// 		counter --;
+	// 		currentDir ="CCW";
+	// 	} else {
+	// 		// Encoder is rotating CW so increment
+	// 		counter ++;
+	// 		currentDir ="CW";
+	// 	}
 
 
-  delay(500);
+    
+	// }
 
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0,0);
-  // display_handler.print("rotations: ");
-  // display_handler.println(rotations);
-  // display_handler.print("steps: ");
-  // display_handler.print(steps);
-  // display_handler.display();
+  //   display_handler.clearDisplay();
+  //   display_handler.setTextSize(1);
+  //   display_handler.setTextColor(SSD1306_WHITE);
+  //   display_handler.setCursor(0,0);
+	// 	display_handler.print("Direction: ");
+	// 	display_handler.println(currentDir);
+	// 	display_handler.print("Counter: ");
+	// 	display_handler.println(counter);
 
+	// 	display_handler.print("currentStateCLK: ");
+	// 	display_handler.println(currentStateCLK);
+	// 	display_handler.print("lastStateCLK: ");
+	// 	display_handler.println(lastStateCLK);
 
-  
+  //   display_handler.display();
 
-  
+	// // Remember last CLK state
+	// lastStateCLK = currentStateCLK;
 
-
-  /*  Servo control  */
-
-  
-  // for (int i = 150; i<=210; i++){
-  //   servo1.write(i);
-  //   delay(5);
-  // }
-
-
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0,0);
-  // display_handler.println("210 degrees");
-  // display_handler.display();
-
-
-  // //servo1.write(210);
-
-
-  // delay(2000);
-
-  // //servo1.write(150);
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0,0);
-  // display_handler.println("150 degrees");
-  // display_handler.display();
-
-  // for (int i = 210; i >= 150; i--){
-  //   servo1.write(i);
-  //   delay(5);
-  // }
-
-
-  // delay(2000);
-
-
-
-
-  /*  DC motor Section  */
-  // {
-
-
-  
-  // int potVal = analogRead(POT_PIN);
-  // int motorVal = map(potVal, 0, 1100, 0, 4096);
-
-  // display_handler.clearDisplay();
-  // display_handler.setTextSize(1);
-  // display_handler.setTextColor(SSD1306_WHITE);
-  // display_handler.setCursor(0,0);
-  // display_handler.print("Pot val: ");
-  // display_handler.println(potVal);
-
-  // display_handler.print("Motor val: ");
-  // display_handler.println(motorVal);
-  // display_handler.display();
-
-  // motor1.stop();
-  // delay(1000);
-
-  // motor1.forward(motorVal);
-  // delay(3000);
-
-  // motor1.stop();
-  // delay(1000);
-
-  // motor1.backward(motorVal);
-  // delay(3000);
-
-  // }
-
+	// // Put in a slight delay to help debounce the reading
+	// delay(10);
 }
 
 
-void rotaryEncoder(){
+void updateEncoder(){
+	// Read the current state of CLK
+	currentStateCLK = digitalRead(CLK);
+  delay(5);
+ 	currentStateDT = digitalRead(DT);
 
-  bool A = digitalRead(ROTARY_A);
-  bool B = digitalRead(ROTARY_B);
+	// If last and current state of CLK are different, then pulse occurred
+	// React to only 1 state change to avoid double count
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
 
-  if (A=B){
-    rotations++;
-  }
-  else {
-    rotations--;
-  }
 
+
+      // display_handler.clearDisplay();
+      // display_handler.setTextSize(1);
+      // display_handler.setTextColor(SSD1306_WHITE);
+      // display_handler.setCursor(0,0);
+      // display_handler.print("In if function");
+      // display_handler.println();
+      // display_handler.println();
+      // display_handler.println();
+      // display_handler.println();
+      // display_handler.display();
+
+
+		// If the DT state is different than the CLK state then
+		// the encoder is rotating CCW so decrement
+		if (currentStateDT != currentStateCLK) {
+			counter--;
+			currentDir ="CCW";
+		} else if(currentStateDT == currentStateCLK) {
+			// Encoder is rotating CW so increment
+			counter++;
+			currentDir ="CW";
+		}
+
+    else {
+
+      currentDir = "broken";
+    }
+
+
+    // display_handler.clearDisplay();
+    // display_handler.setTextSize(1);
+    // display_handler.setTextColor(SSD1306_WHITE);
+    // display_handler.setCursor(0,0);
+		// display_handler.print("Direction: ");
+		// display_handler.println(currentDir);
+		// display_handler.print("Counter: ");
+		// display_handler.println(counter);
+
+		// // display_handler.print("currentStateCLK: ");
+		// // display_handler.println(currentStateCLK);
+		// // display_handler.print("lastStateCLK: ");
+		// // display_handler.println(lastStateCLK);
+
+    // display_handler.display();
+
+	}
+
+	// Remember last CLK state
+	lastStateCLK = currentStateCLK;
 }
