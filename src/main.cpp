2afebe83a913038050656da0e3e0dd2104397caa
JamesJ-C <james@hjcoffinco.com>
2024-07-13 12:48:13 -0700

    Cleaned up code & documented. Speed functionality is still not working. Will fix later

--- src/main.cpp
+++ src/main.cpp
@@ -142,27 +142,34 @@ class Motor {
 
 class RotaryEncoder {
 
-
   private:
+
+
+  const int clicksPerRotation = 20; //depending on each encoder
 
   PinName pinA;
   PinName pinB;
 
 
-  const int clicksPerRotation = 20; //depending on each encoder
+  /*  for calculating direction. This value seems to make 0 position work on startup 
+  otherwise there would be an offset on increment by 2 */
+  int lastEncoded = 0b11; 
+  int increments = 0; //net clicks offset from 0
+  int previousIncrement; //net clicks at the previous time stamp
+  double angularVelocity = 0; 
 
-  int lastEncoded = 0b11; //for calculating direction. This value seems to make 0 position work on startup
-                          // otherwise there would be an offset on increment by 2
-  int increments = 0; //increments in a direction
-  int previousIncrement;//for calculating velocity
-  int angularVelocity; 
-
-  unsigned long lastUpdateTime = 0;//for velocity
-  int deltaT; //for velocity
+  unsigned long lastUpdateTime = 0; //time at which the ISR was last called
+  int deltaT; //ftime between the most recent ISR calls
   
   
   public:
 
+  /**
+   * @brief Construct a new Rotary Encoder object
+   * 
+   * @param pinA pin attached to one terminal of the encoder
+   * @param pinB pin attached to the other terminal of the encoder
+   */
   RotaryEncoder(PinName pinA, PinName pinB) : pinA(pinA), pinB(pinB) {}
 
   /**
@@ -189,22 +196,48 @@ class RotaryEncoder {
   int getIncrements(){
     return increments;
   }
-  int getSpeed(){
+  /**
+   * @brief Get the Speed measured by the encoder
+   * 
+   * @return int speed
+   */
+  double getSpeed(){
+    this->updateSpeed();
     return angularVelocity;
   }
 
+  
+  /**
+   * @brief Updates the time of the most recent ISR call 
+   * and the time between the 3 most recent calls
+   * 
+   * @param time current time
+   */
   void updateTime(unsigned long time){
 
     deltaT = time - this->lastUpdateTime;
     this->lastUpdateTime = time;
   }
 
+  
+  /**
+   * @brief updates the speed of the encoder. Must be called after the ISR call, but before the next ISR call
+   * 
+   */
   void updateSpeed(){
 
-    angularVelocity = ( (this->increments - this->previousIncrement) / deltaT ) / clicksPerRotation;
+
+    //THIS FUNCTION DOES NOT WORK
+    angularVelocity =  ( (double) (this->increments - this->previousIncrement) / deltaT ) / (double) clicksPerRotation;
 
   }
 
+  /**
+   * @brief old function not needed. Still would like to keep for the time being
+   * 
+   * @param Aa 
+   * @param Bb 
+   */
   void updateEncoder(bool Aa, bool Bb){
 
     // bool A = digitalRead(ROTARY_A);
@@ -229,6 +262,11 @@ class RotaryEncoder {
 
   }
 
+  
+  /**
+   * @brief Updates the encoder object. Should be called from an ISR
+   * 
+   */
   void updateEncoder(){
 
     bool A = digitalRead(ROTARY_A);
@@ -322,7 +360,10 @@ void loop() {
 	display_handler.println(encoder1.getIncrements() );
 
 
-  
+  display_handler.print("Obj speed: ");
+	display_handler.println(encoder1.getSpeed() );
+
+
   display_handler.display();
 
 
