6cf49351215bbb9fcf3efd867e0100396a72ad87
iellasn <74729167+iellasn@users.noreply.github.com>
2024-07-22 19:31:46 -0700

    platess!

--- src/PlateMovement.cpp
+++ src/PlateMovement.cpp
@@ -1,11 +1,6 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <Servo.h>
-
-#include <string>
-
 #include <HardwareSerial.h>
-
 #include "RotaryEncoder.h"
 #include "Motor.h"
 #include "robotConstants.h"
@@ -18,196 +13,25 @@
 HardwareSerial SerialPort(USART3);
 String msg;
 
-/*  Function Declerations  */
-void updateEncoder();
-void ISRUpdateEncoder();
-void ISRButton();
-
-
-//NEED TO FIX THIS VARIABLE
-bool buttonPressed = false;
-//NEED TO FIX THIS VARIABLE
-
-
-/*  PID Control Values  */
-
-int setVal = 32;
-
-int measuredVal;
-
-double plateError = 0.0;
-double lastPlateError = 0.0;
-
-double MAX_I = 140;
-
-double p_plate_Val, d_plate_Val, i_plate_Val;
-
-double g_plate_Val ;
-
-
-/*  Object declerations  */
-
-encoder::RotaryEncoder encoder1(PB_8, PB_9);
-movement::Motor MotorL(MotorL_P1, MotorL_P2);//, &encoder1);
-movement::Motor MotorR(MotorR_P1, MotorR_P2);
-
-
-void setup() {
-
-  SerialPort.begin(115200);
-
-	// Setup Serial Monitor
-	Serial.begin(115200);
-  Serial.println("Hello" + String(BOARD_NAME));
-
-  /*  Pot Pin  */
-  pinMode(POT_PIN, INPUT);
-
-
-  /*  Motor Pins  */
-  pinMode(MotorL.getPinA(), OUTPUT);
-  pinMode(MotorL.getPinB(), OUTPUT);
-
-  pinMode(MotorR.getPinA(), OUTPUT);
-  pinMode(MotorR.getPinB(), OUTPUT);
-
-  MotorL.off();
-  MotorR.off();
-  delay(500);
-  MotorL.forward(3000);
-  delay(100);
-  MotorL.off();
-  MotorR.forward(3000);
-  delay(100);
-  MotorR.off();
-
-  /*  Encoders  */
-	pinMode(ROTARY_A, INPUT);
-	pinMode(ROTARY_B, INPUT);
-  // pinMode(encoder1.getPinA(), INPUT);
-	// pinMode(encoder1.getPinB(), INPUT);
-
-  pinMode(BUTTON_PIN, INPUT);
-
-
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), ISRButton, RISING);
-
-	attachInterrupt(digitalPinToInterrupt(ROTARY_A), ISRUpdateEncoder, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(ROTARY_B), ISRUpdateEncoder, CHANGE);
-
-}
-
-
-void loop() {
-
-  if (SerialPort.available() > 0){
-    msg = SerialPort.readString();
-  }
-
-  SerialPort.print("motor.Obj Counter: ");
-	SerialPort.println(MotorL.encoder->getIncrements() );
-
-  Serial.print("motor.Obj Counter: ");
-	Serial.println(MotorL.encoder->getIncrements() );
-
-  int setVal = 32;
-  int measuredVal;
-
-  int readVal = msg.toInt();
-
-  setVal = map(readVal, 0, 1023, -500, 500);
-
-  measuredVal = MotorL.encoder->getIncrements();
-
-  plateError = setVal - measuredVal;
-  
-
-  double PLATE_PID_TOTAL_GAIN = 1.0;
-  double P_PLATE_GAIN = 0.55;//1.4 goes very slowl
-  double I_PLATE_GAIN = 0.0;
-  double D_PLATE_GAIN = 0;
-
-  p_plate_Val = P_PLATE_GAIN *plateError;
-  d_plate_Val = D_PLATE_GAIN * (plateError - lastPlateError);
-  i_plate_Val = I_PLATE_GAIN *plateError + i_plate_Val; //const *plateError + previous int value
-  if (i_plate_Val > MAX_I) {i_plate_Val = MAX_I;}
-  if (i_plate_Val < -MAX_I) {i_plate_Val = -MAX_I;}
-
-  g_plate_Val = PLATE_PID_TOTAL_GAIN * ( p_plate_Val + i_plate_Val + d_plate_Val ); 
-  lastPlateError =plateError; 
-
-  //Do motor code here
- 
-
-  /*  SerialPort & Serial Monitor prints  */
-  {
-
-  SerialPort.println("g: " + String( g_plate_Val ) );
-  Serial.println("g: " + String( g_plate_Val ) );
-
-  SerialPort.println( "error: " + String(plateError ));
-  SerialPort.println( "p: " + String( p_plate_Val ));
-  Serial.println( "error: " + String(plateError ));
-  Serial.println( "p: " + String( p_plate_Val ));
-
-  }
-
-}
-
-/**
- * @brief function attached to RotaryA and RotaryB to update encoder values
- * 
- */
-void ISRUpdateEncoder(){
-
-  bool A = digitalRead(ROTARY_A);
-  bool B = digitalRead(ROTARY_B);
-
-  encoder1.updateEncoder(A, B);
-  encoder1.updateTime( millis() );
-
-}
-
-
-/**
- * @brief function for reading the debounced button press values.
- * 
- */
-void ISRButton() {
-
-  //  Serial.print("inside the interrupt");
-
-  encoder1.resetIncrement();
-  //delay(100);
-  buttonPressed = true;
-
-  //MotorL.buttonPressed = true;
-
-}
-
-#include <Arduino.h>
-#include "RotaryEncoder.h"
-#include "Motor.h"
-#include "robotConstants.h"
-
-#define PLATE_MOTOR_PIN_A PB1
-#define PLATE_MOTOR_PIN_B PB2
-#define PLATE_ENCODER_PIN_A PB
-#define PLATE_ENCODER_PIN_B PB
-#define LIMIT_SWITCH_LEFT PB
-#define LIMIT_SWITCH_RIGHT PB
+#define PLATE_MOTOR_PIN_A PB_8
+#define PLATE_MOTOR_PIN_B PB_9
+#define PLATE_ENCODER_PIN_A PB_1
+#define PLATE_ENCODER_PIN_B PB_2
+#define LIMIT_SWITCH_LEFT PB12
+#define LIMIT_SWITCH_RIGHT PB15
+#define SERVOPIN PB13
 
 movement::Motor motor(PLATE_MOTOR_PIN_A, PLATE_MOTOR_PIN_B);
-encoder::RotaryEncoder encoder(PLATE_ENCODER_PIN_A, PLATE_ENCODER_PIN_B);
+encoder::RotaryEncoder plateencoder(PLATE_ENCODER_PIN_A, PLATE_ENCODER_PIN_B);
 
-volatile int encoderPosition = 0; // volatile tells compllier that the vallue can be changed by smth outside of normal program flow like ISR or external hardware
+Servo plateservo; 
+
+volatile int encoderPosition = 0; // volatile tells compllier that the value can be changed by smth outside of normal program flow like ISR or external hardware
 bool movingRight = true; 
 
-
-//functions 
-
-void updateEncoder(); 
+//function prototypes
 void checkLimitSwitches(); 
+void ISRUpdateEncoder();
 
 void setup(){
 
@@ -217,28 +41,46 @@ void setup(){
   pinMode(PLATE_MOTOR_PIN_B, OUTPUT);
 
  //Encoder Pins 
-  pinMode(PLATE_ENCODER_PIN_A, INPUT_PULLUP)
-  pinMode(PLATE_ENCODER_PIN_B, INPUT_PULLUP)
-  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_A), PLATE_ENCODER_PIN_A.updateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_B), PLATE_ENCODER_PIN_B.updateEncoder, CHANGE);
+  pinMode(PLATE_ENCODER_PIN_A, INPUT_PULLUP);
+  pinMode(PLATE_ENCODER_PIN_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_A), ISRUpdateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_B), ISRUpdateEncoder, CHANGE);
   //Sets up interrupt to call 'updateEncoder' ISR function whenever theres a change to the encoder pin 
   
   //Limit Switch Pins 
   pinMode(LIMIT_SWITCH_LEFT, INPUT);
   pinMode(LIMIT_SWITCH_RIGHT, INPUT);
 
-  motor.forward(2000); 
-}
+  plateservo.attach(SERVOPIN);
+  plateservo.write(90);
 
+  motor.forward(4096); 
+}
 void loop(){
-  Serial.print("Encoder Position: ");
-  Serial.println(encoderPosition);
-  delay(100);
-}
-void updateEncoder(){
-
+  //Serial.print("Encoder Position: ");
+  //Serial.println(encoderPosition);
+  //delay(100);
+  checkLimitSwitches();
 }
 
-void checkLimitSwitches(){
+  void checkLimitSwitches() {
+  if (digitalRead(LIMIT_SWITCH_LEFT) == LOW && !movingRight) {
+    SerialPort.println("off1");
+    motor.off();
+  } else if (digitalRead(LIMIT_SWITCH_RIGHT) == LOW && movingRight) {
+    motor.off();
+    SerialPort.println("off1");
+    delay(10000);
+    motor.backward(4096); // Start moving backward
+    movingRight = false;
+  }
+}
 
+void ISRUpdateEncoder(){
+
+  bool A = digitalRead(ROTARY_A);
+  bool B = digitalRead(ROTARY_B);
+
+  plateencoder.updateEncoder(A, B);
+  plateencoder.updateTime( millis());
 }
