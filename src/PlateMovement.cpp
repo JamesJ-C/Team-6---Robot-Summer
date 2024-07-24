19455a0956ebd9990a8e8033e93c8045e483daf0
iellasn <74729167+iellasn@users.noreply.github.com>
2024-07-24 10:41:09 -0700

    plate movement commit

--- src/PlateMovement.cpp
+++ src/PlateMovement.cpp
@@ -19,23 +19,34 @@ String msg;
 #define PLATE_ENCODER_PIN_B PB_2
 #define LIMIT_SWITCH_LEFT PB12
 #define LIMIT_SWITCH_RIGHT PB15
-#define SERVOPIN PB13
+#define PLATESERVO PB12
+#define CLAWSERVO1 PB13 
+#define CLAWSERVO2 PB14
+#define CLAW_INITIAL_L 90
+#define CLAW_INITIAL_R 90
+#define CLAW_OPEN_L 60
+#define CLAW_OPEN_R 150
 
 movement::Motor motor(PLATE_MOTOR_PIN_A, PLATE_MOTOR_PIN_B);
 encoder::RotaryEncoder plateencoder(PLATE_ENCODER_PIN_A, PLATE_ENCODER_PIN_B);
 
 Servo plateservo; 
+Servo clawservoleft; 
+Servo clawservoright;
 
 volatile int encoderPosition = 0; // volatile tells compllier that the value can be changed by smth outside of normal program flow like ISR or external hardware
 bool movingRight = true; 
 
 //function prototypes
-void checkLimitSwitches(); 
+void checkLimitSwitches(); // switch goes high when pressed
 void ISRUpdateEncoder();
+void ISRButton();
+void openClaw();
+void closeClaw();
 
 void setup(){
-
   Serial.begin(115200); 
+  Serial.println(); 
   //Motor Pins
   pinMode(PLATE_MOTOR_PIN_A, OUTPUT); 
   pinMode(PLATE_MOTOR_PIN_B, OUTPUT);
@@ -45,33 +56,48 @@ void setup(){
   pinMode(PLATE_ENCODER_PIN_B, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_A), ISRUpdateEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_B), ISRUpdateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_LEFT),ISRButton, RISING);
+  attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_RIGHT),ISRButton, RISING);
+
   //Sets up interrupt to call 'updateEncoder' ISR function whenever theres a change to the encoder pin 
   
   //Limit Switch Pins 
   pinMode(LIMIT_SWITCH_LEFT, INPUT);
   pinMode(LIMIT_SWITCH_RIGHT, INPUT);
 
-  plateservo.attach(SERVOPIN);
+  plateservo.attach(PLATESERVO);
   plateservo.write(90);
 
-  motor.forward(4096); 
+  clawservoleft.attach(CLAWSERVO1); 
+  clawservoright.attach(CLAWSERVO2);
+
+//Initial Claw Position
+  clawservoleft.write(CLAW_INITIAL_L);
+  clawservoright.write(CLAW_INITIAL_R);
+
 }
 void loop(){
   //Serial.print("Encoder Position: ");
   //Serial.println(encoderPosition);
   //delay(100);
-  checkLimitSwitches();
-}
+  // checkLimitSwitches();
 
-  void checkLimitSwitches() {
-  if (digitalRead(LIMIT_SWITCH_LEFT) == LOW && !movingRight) {
-    SerialPort.println("off1");
+if(movingRight == true){
+  motor.backward(3500);
+}
+else{
+  motor.forward(3500);
+}
+  //checking boolean if its one state move in direction if not ,ove in another and interrupt for when stwitch is preessed and th state is sqwitches 
+
+}
+  void checkLimitSwitches(){
+  if (digitalRead(LIMIT_SWITCH_LEFT) == HIGH && !movingRight) {
     motor.off();
-  } else if (digitalRead(LIMIT_SWITCH_RIGHT) == LOW && movingRight) {
+  } else if (digitalRead(LIMIT_SWITCH_RIGHT) == HIGH && movingRight) {
     motor.off();
-    SerialPort.println("off1");
-    delay(10000);
-    motor.backward(4096); // Start moving backward
+    delay(2000);
+    motor.forward(3500); // Start moving backward
     movingRight = false;
   }
 }
@@ -83,4 +109,26 @@ void ISRUpdateEncoder(){
 
   plateencoder.updateEncoder(A, B);
   plateencoder.updateTime( millis());
+}
+
+int count = 0;
+
+void ISRButton(){
+  motor.off(); 
+  movingRight = !movingRight; 
+  // Serial.println(String(count) + "ISR function");
+  delay(2000);
+}
+
+
+
+void 
+
+
+void openclaw(){
+  clawservoleft.write(CLAW_OPEN_L);
+  clawservoright.write(CLAW_OPEN_R); 
+}
+
+void closeclaw(){
 }
