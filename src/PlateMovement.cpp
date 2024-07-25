e944ca9325f653d66309769fe50dd7f13f12c03b
iellasn <74729167+iellasn@users.noreply.github.com>
2024-07-25 12:36:38 -0700

    commit plateNew

--- src/PlateMovement.cpp
+++ src/PlateMovement.cpp
@@ -37,12 +37,33 @@ Servo clawservoright;
 volatile int encoderPosition = 0; // volatile tells compllier that the value can be changed by smth outside of normal program flow like ISR or external hardware
 bool movingRight = true; 
 
+enum State {
+  IDLE,
+  MOVING_RIGHT,
+  MOVING_LEFT,
+  ACCEPT_BURGER,
+  SERVE_BURGER,
+  FINISH_SERVE,
+  FINISH_ACCEPT,
+  TRAVEL_MODE
+};
+State currentState = IDLE; 
+
+
 //function prototypes
-void checkLimitSwitches(); // switch goes high when pressed
-void ISRUpdateEncoder();
-void ISRButton();
+// void checkLimitSwitches(); // switch goes high when pressed
+// void ISRUpdateEncoder();
+// void ISRButton();
 void openClaw();
 void closeClaw();
+void movePlateRight();
+void movePlateLeft();
+void stopPlate();
+void sweepObject();
+void clawNeutral(); 
+void movePlateMiddlefromRight(); 
+void movePlateMiddlefromLeft(); 
+void checkState(); 
 
 void setup(){
   Serial.begin(115200); 
@@ -54,44 +75,62 @@ void setup(){
  //Encoder Pins 
   pinMode(PLATE_ENCODER_PIN_A, INPUT_PULLUP);
   pinMode(PLATE_ENCODER_PIN_B, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_A), ISRUpdateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_B), ISRUpdateEncoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_LEFT),ISRButton, RISING);
-  attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_RIGHT),ISRButton, RISING);
-
+  // attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_A), ISRUpdateEncoder, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(PLATE_ENCODER_PIN_B), ISRUpdateEncoder, CHANGE);
   //Sets up interrupt to call 'updateEncoder' ISR function whenever theres a change to the encoder pin 
   
   //Limit Switch Pins 
   pinMode(LIMIT_SWITCH_LEFT, INPUT);
   pinMode(LIMIT_SWITCH_RIGHT, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_LEFT),ISRButton, RISING);
+  // attachInterrupt(digitalPinToInterrupt(LIMIT_SWITCH_RIGHT),ISRButton, RISING);
 
+  //Attaching servos 
   plateservo.attach(PLATESERVO);
-  plateservo.write(90);
-
   clawservoleft.attach(CLAWSERVO1); 
   clawservoright.attach(CLAWSERVO2);
 
 //Initial Claw Position
-  clawservoleft.write(CLAW_INITIAL_L);
-  clawservoright.write(CLAW_INITIAL_R);
+  clawNeutral(); 
 
 }
 void loop(){
-  //Serial.print("Encoder Position: ");
-  //Serial.println(encoderPosition);
-  //delay(100);
-  // checkLimitSwitches();
+  checkState(); 
+switch (currentState)
+{
+case IDLE:
+  break;
 
-if(movingRight == true){
-  motor.backward(3500);
-}
-else{
-  motor.forward(3500);
-}
-  //checking boolean if its one state move in direction if not ,ove in another and interrupt for when stwitch is preessed and th state is sqwitches 
+case MOVING_RIGHT:
+  movePlateRight();
+  break; 
 
+case SERVE_BURGER:
+  stopPlate();
+  sweepObject(); 
+  currentState = FINISH_SERVE; 
+  break; 
+
+case FINISH_SERVE:
+  clawNeutral(); 
+  movePlateMiddlefromRight(); 
+  currentState = IDLE; //IDLE STATE is with plate in the middle and claw in neutral position 
+
+case MOVING_LEFT:
+  movePlateLeft(); 
+
+case ACCEPT_BURGER:
+  stopPlate();
+  openClaw();
+
+case TRAVEL_MODE:
+  closeClaw(); 
+  delay(2000);
+  void movePlateMiddlefromLeft();
+  currentState = IDLE; 
+  }
 }
-  void checkLimitSwitches(){
+void checkLimitSwitches(){
   if (digitalRead(LIMIT_SWITCH_LEFT) == HIGH && !movingRight) {
     motor.off();
   } else if (digitalRead(LIMIT_SWITCH_RIGHT) == HIGH && movingRight) {
@@ -102,33 +141,84 @@ else{
   }
 }
 
-void ISRUpdateEncoder(){
-
-  bool A = digitalRead(ROTARY_A);
-  bool B = digitalRead(ROTARY_B);
-
-  plateencoder.updateEncoder(A, B);
-  plateencoder.updateTime( millis());
+void checkState(){
+  if(inservingarea){
+  currentState = MOVING_RIGHT;
+} else if(digitalRead(LIMIT_SWITCH_RIGHT == HIGH)){
+  currentState = SERVE_BURGER; 
+} else if(atcuttingarea){
+  currentState = MOVING_LEFT; 
+} else if(digitalRead(LIMIT_SWITCH_RIGHT == HIGH)){
+  currentState = ACCEPT_BURGER; 
+} else if(wallesaysdone){
+  currentState = TRAVEL_MODE; 
+}
 }
 
-int count = 0;
+// void ISRUpdateEncoder(){
 
-void ISRButton(){
+//   bool A = digitalRead(ROTARY_A);
+//   bool B = digitalRead(ROTARY_B);
+
+//   plateencoder.updateEncoder(A, B);
+//   plateencoder.updateTime( millis());
+// }
+
+// void ISRButton(){
+//   motor.off(); 
+//   movingRight = !movingRight; 
+//   // Serial.println(String(count) + "ISR function");
+//   //delay(2000);
+// }
+
+void stopPlate(){
+  motor.stop(); 
+}
+
+void movePlateRight(){
+  motor.backward(3500); 
+}
+
+void movePlateLeft(){
+  motor.forward(3500); 
+}
+
+void expandPlate(){
+  plateservo.write(90);
+  delay(1000); 
+}
+
+void movePlateMiddlefromRight(){
+  motor.forward(3500);
+  delay(2500); 
   motor.off(); 
-  movingRight = !movingRight; 
-  // Serial.println(String(count) + "ISR function");
-  delay(2000);
+}
+
+void movePlateMiddlefromLeft(){
+  motor.backward(3500); 
+  delay(2500);
+  motor.off();
+}
+
+void openClaws(){
+  clawservoleft.write(0); 
+  clawservoright.write(180);
+}
+
+void closeClaws(){
+  clawservoleft.write(90); 
+  clawservoright.write(90);
+}
+
+void clawNeutral(){
+  clawservoleft.write(30);
+  clawservoright.write(40); 
+}
+
+void sweepObject(){
+  clawservoright.write(180); 
+  delay(1000);
+  clawservoleft.write(180); 
 }
 
 
-
-void 
-
-
-void openclaw(){
-  clawservoleft.write(CLAW_OPEN_L);
-  clawservoright.write(CLAW_OPEN_R); 
-}
-
-void closeclaw(){
-}
