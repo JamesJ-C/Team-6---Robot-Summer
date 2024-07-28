9594a8a34765ed9325784cb1b8c24f44b61cd0b3
JamesJ-C <james@hjcoffinco.com>
2024-07-28 13:34:32 -0700

    added new class doc

---
+++ lib/RobotSubSystem/RobotSubSystem.h
@@ -0,0 +1,25 @@
+#ifndef ROBOT_SUB_SYSTEM
+#define ROBOT_SUB_SYSTEM
+
+
+#include <Arduino.h>
+#include <RotaryEncoder.h>
+
+#ifdef ESP32
+    #include <espConstants.h>
+#endif
+
+#ifndef ESP32
+    #include <bpConstants.h>
+#endif
+
+class classname {
+
+    private:
+
+    public:
+
+
+};
+
+#endif
