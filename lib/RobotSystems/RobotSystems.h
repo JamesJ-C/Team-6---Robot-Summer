9014c1f8376622dd0f6645506a0ed02ffc48d79c
JamesJ-C <james@hjcoffinco.com>
2024-07-31 15:55:11 -0700

    robot tuning

--- lib/RobotSystems/RobotSystems.h
+++ lib/RobotSystems/RobotSystems.h
@@ -110,9 +110,9 @@ private:
 
         const int forwardMidMotorSpeed = 3300;
         const double FORWARD_LOOP_GAIN = 1.0;
-        const double FORWARD_P_GAIN = 0.55;
+        const double FORWARD_P_GAIN = 1.2;
         const double FORWARD_I_GAIN = 0.0;
-        const double FORWARD_D_GAIN = 1.36;
+        const double FORWARD_D_GAIN = 1.4;
         const double MAX_FORWARD_I = 1400.0;
 
         double backward_p;
@@ -123,9 +123,9 @@ private:
 
         const int backwardMidMotorSpeed = 3300;
         const double BACKWARD_LOOP_GAIN = 1.0;
-        const double BACKWARD_P_GAIN = 0.55;
+        const double BACKWARD_P_GAIN = 0.8;
         const double BACKWARD_I_GAIN = 0.0;
-        const double BACKWARD_D_GAIN = 1.36;
+        const double BACKWARD_D_GAIN = 1.0;
         const double MAX_BACKWARD_I = 1400.0;
 
         double ir_p;
